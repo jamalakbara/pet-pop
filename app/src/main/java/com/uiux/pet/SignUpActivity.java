85137d203026eaef9ad15afe883ab21e08ebb947
akbar <jamalakbaralam@gmail.com>
2019-08-28 11:58:01 +0700

    Home 1/4

--- app/src/main/java/com/uiux/pet/SignUpActivity.java
+++ app/src/main/java/com/uiux/pet/SignUpActivity.java
@@ -2,13 +2,29 @@ package com.uiux.pet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_to_login = new Intent(SignUpActivity.this, LogInActivity.class);
+                startActivity(go_to_login);
+            }
+        });
+
     }
 }
